78ab2e5c958e3b171349a50d85b3b12a62b9f0b6
notsatria <buanasatriaa@gmail.com>
2024-05-21 13:58:48 +0700

    feat: login api integration

--- app/src/main/java/com/bangkit/gethub/data/repositories/AuthRepository.kt
+++ app/src/main/java/com/bangkit/gethub/data/repositories/AuthRepository.kt
@@ -1,12 +1,17 @@
 package com.bangkit.gethub.data.repositories
 
 import com.bangkit.gethub.data.remote.response.ApiResponse
+import com.bangkit.gethub.data.remote.response.auth.LoginResponse
 import com.bangkit.gethub.data.remote.retrofit.ApiService
 
 class AuthRepository private constructor(private val apiService: ApiService) {
 
     suspend fun register(fullname: String, email: String, password: String): ApiResponse {
         return apiService.register(fullname, email, password)
+    }
+
+    suspend fun login(email: String, password: String): LoginResponse {
+        return apiService.login(email, password)
     }
 
     companion object {
