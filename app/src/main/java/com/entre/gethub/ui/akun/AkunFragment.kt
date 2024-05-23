175e8599b094cb48f628bc10aad52e1fe16276ee
MDanishRabbani <danishrabbani1806@gmail.com>
2024-05-23 14:16:03 +0700

    feat: update AkunFragment and AkunViewModel

---
+++ app/src/main/java/com/entre/gethub/ui/akun/AkunFragment.kt
@@ -0,0 +1,133 @@
+package com.entre.gethub.ui.akun
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.entre.gethub.R
+import com.entre.gethub.data.Result
+import com.entre.gethub.ui.auth.LoginActivity
+import com.entre.gethub.databinding.FragmentAkunBinding
+import com.entre.gethub.utils.ViewModelFactory
+
+
+class AkunFragment : Fragment() {
+
+    private var _binding: FragmentAkunBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var akunViewModel: AkunViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initViewModel()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentAkunBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        setupView()
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupView() {
+        with(binding) {
+            framesettingkeluar.setOnClickListener {
+                showLogoutDialog()
+            }
+        }
+
+        getUserData()
+
+    }
+
+    private fun getUserData() {
+        akunViewModel.getUserProfile().observe(viewLifecycleOwner) { result ->
+            if (result != null) {
+                when (result) {
+                    is Result.Loading -> showLoading(true)
+                    is Result.Success -> {
+                        val user = result.data.data
+                        showLoading(false)
+                        println("Photo URL: ${user?.photo}") // Menampilkan URL gambar
+                        with(binding) {
+                            Glide.with(requireContext())
+                                .load(user?.photo)
+                                .placeholder(R.drawable.profilepic1)
+                                .into(ivUserProfilePicture)
+                            tvUserFullname.text = user?.fullName
+                            tvUserProfession.text = user?.profession
+                        }
+                    }
+
+                    is Result.Error -> {
+                        showLoading(false)
+                        showToast(result.error)
+                    }
+
+                    else -> {
+                        showLoading(false)
+                        showToast("Terjadi Kesalahan")
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun initViewModel() {
+        val factory = ViewModelFactory.getInstance(requireContext())
+        akunViewModel = ViewModelProvider(requireActivity(), factory)[AkunViewModel::class.java]
+    }
+
+    private fun showLogoutDialog() {
+        LogoutDialogFragment(onLogout = {
+            logout()
+        }).show(parentFragmentManager, "LogoutDialog")
+    }
+
+
+    private fun logout() {
+        akunViewModel.logout().apply {
+            navigateToLoginActivity()
+        }
+    }
+
+    private fun navigateToLoginActivity() {
+        startActivity(
+            Intent(requireContext(), LoginActivity::class.java)
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        )
+    }
+
+    private fun navigateToActivity(activity: AppCompatActivity) {
+        startActivity(Intent(requireContext(), activity::class.java))
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
+}
