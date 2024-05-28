913235c35f239fb33b6d0fcf7882ed1c074c5fb0
MDanishRabbani <danishrabbani1806@gmail.com>
2024-05-28 15:27:19 +0700

    feat: user add field wajib diisi

--- app/src/main/java/com/entre/gethub/ui/home/mygethub/link/HomeKelolaMyGethubTambahLinkViewModel.kt
+++ app/src/main/java/com/entre/gethub/ui/home/mygethub/link/HomeKelolaMyGethubTambahLinkViewModel.kt
@@ -1,12 +1,14 @@
 package com.entre.gethub.ui.home.mygethub.link
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.entre.gethub.data.Result
 import com.entre.gethub.data.remote.response.ApiResponse
 import com.entre.gethub.data.remote.response.LinkResponse
+import com.entre.gethub.data.remote.response.products.ProductResponse
 import com.entre.gethub.data.repositories.LinkRepository
 import com.google.gson.Gson
 import kotlinx.coroutines.launch
@@ -16,10 +18,12 @@ class HomeKelolaMyGethubTambahLinkViewModel(private val linkRepository: LinkRepo
     ViewModel() {
 
     private val result = MutableLiveData<Result<LinkResponse>>()
+    private val addLinkResult = MediatorLiveData<Result<LinkResponse>>()
 
     fun addLink(category: String, link: String): LiveData<Result<LinkResponse>> {
         if (category.isEmpty() || link.isEmpty()) {
-            result.value = Result.Error("Semua bidang harus diisi")
+            addLinkResult.value = Result.Error("Semua bidang harus diisi")
+            return addLinkResult
         }
         viewModelScope.launch {
             try {
